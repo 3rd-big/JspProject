6b6ffa8b3b18ad7cf649909e8c9c97d53fe128f2
Sunjung Jang <ssunjz331@gmail.com>
2020-08-31 14:28:12 +0900

    testpush

--- JspProject.sql
+++ JspProject.sql
@@ -65,3 +65,6 @@ create SEQUENCE seq_product_size;
 create SEQUENCE seq_product_image;
 create SEQUENCE seq_shop_product;
 create SEQUENCE seq_notice;
+create SEQUENCE seq_product_order;
+
+alter table product_order add p_size varchar2(3);
